2513c0c589bef46dd748d9cf82761866bf84fb47
mynus <matheusgsandri@gmail.com>
2022-03-09 16:27:35 -0300

    feat: first commit

---
+++ app/src/main/java/com/mynus01/firesns/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mynus01.firesns
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
